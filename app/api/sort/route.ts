b721acde84033206383ec658df6e84f33579aefc
Aditya Rawat <adityarawatnew2487@gmail.com>
2025-07-04 12:31:10 +0530

    sorting added to the sort

---
+++ app/api/sort/route.ts
@@ -0,0 +1,23 @@
+import { connectDb } from "@/lib/mongoose";
+import User from "@/models/user";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+  try {
+    await connectDb();
+    const users = await User.aggregate([
+      {
+        $sort: {
+          age: 1,
+        },
+      },
+    ]);
+    return NextResponse.json({ users }, { status: 200 });
+  } catch (error) {
+    console.log((error as Error).message);
+    return NextResponse.json(
+      { message: "Internal server issue" },
+      { status: 500 }
+    );
+  }
+}
