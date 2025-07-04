3315acac567709636d0f609172a9a445e59530b0
Aditya Rawat <adityarawatnew2487@gmail.com>
2025-07-04 12:19:38 +0530

    made the profile

--- app/api/profile/route.ts
+++ app/api/profile/route.ts
@@ -1,12 +1,35 @@
 import { connectDb } from "@/lib/mongoose";
-import { NextRequest } from "next/server";
+import User from "@/models/user";
+import mongoose from "mongoose";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
-    try {
-        await connectDb()
-        
-    } catch (error) {
-        
-    }
-    
+  try {
+    await connectDb();
+
+    const id = req.nextUrl.searchParams.get("id");
+
+    // Create match stage conditionally
+    const matchStage = id
+      ? [{ $match: { _id: new mongoose.Types.ObjectId(id) } }]
+      : [];
+
+    const users = await User.aggregate([
+      ...matchStage,
+      {
+        $project: {
+          _id: 1,
+          fullname: { $concat: ["$firstname", " ", "$lastname"] },
+        },
+      },
+    ]);
+
+    return NextResponse.json({ users }, { status: 200 });
+  } catch (error) {
+    console.log((error as Error).message);
+    return NextResponse.json(
+      { message: "Internal server issue" },
+      { status: 500 }
+    );
+  }
 }
