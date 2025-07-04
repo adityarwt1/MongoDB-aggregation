c73c75517895aa4d66de25faf332a2ed573c41f0
Aditya Rawat <adityarawatnew2487@gmail.com>
2025-07-04 12:45:30 +0530

    countt the total document by the second method

---
+++ app/api/count2/route.ts
@@ -0,0 +1,17 @@
+import { connectDb } from "@/lib/mongoose";
+import User from "@/models/user";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+  try {
+    await connectDb();
+    const users = await User.aggregate([{ $count: "totalDocs" }]);
+    return NextResponse.json({ users }, { status: 200 });
+  } catch (error) {
+    console.log((error as Error).message);
+    return NextResponse.json(
+      { message: "Internal server issue" },
+      { status: 500 }
+    );
+  }
+}
